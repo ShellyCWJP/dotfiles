2bc9b7a73b77230c4e9ab7a5101f2e3f2c9d934c
Kazuto Sueyoshi <shelly@wraithgene.com>
2025-12-03 09:08:43 +0900

    feat: 環境依存設定用の local.lua を追加

--- packages/nvim/.config/nvim/init.lua
+++ packages/nvim/.config/nvim/init.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("config")
+
+local local_config = vim.fn.expand("~/.config/nvim/local.lua")
+if vim.fn.filereadable(local_config) == 1 then
+  dofile(local_config)
+end
