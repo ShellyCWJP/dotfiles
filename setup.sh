a1143622a30aa111fc9a197f834445358ca2ae45
Kazuto Sueyoshi <shelly@wraithgene.com>
2026-02-11 18:29:17 +0900

    remove: Jetpack installation

--- setup.sh
+++ setup.sh
@@ -25,6 +25,3 @@ fi
 
 echo "Symlinking dotfiles..."
 stow -v -d ~/dotfiles/packages -t ~ alacritty zsh nvim tmux git 
-
-# NeoVim Plugins
-curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
