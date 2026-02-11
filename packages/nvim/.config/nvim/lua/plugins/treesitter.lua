ee1cb4159fdeb0e898e67cf45626e88030bbc752
Kazuto Sueyoshi <shelly@wraithgene.com>
2026-02-11 21:43:42 +0900

    fix: treesitter が動作しないバグを修正

--- packages/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ packages/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    ensure_installed = {
+  config = function()
+    require("nvim-treesitter").setup()
+
+    local ensure_installed = {
       "lua",
       "javascript",
       "typescript",
@@ -16,9 +18,21 @@ return {
       "bash",
       "vim",
       "vimdoc",
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
-  main = "nvim-treesitter.configs",
+    }
+
+    local installed = require("nvim-treesitter").get_installed()
+    local to_install = vim.tbl_filter(function(lang)
+      return not vim.tbl_contains(installed, lang)
+    end, ensure_installed)
+    if #to_install > 0 then
+      require("nvim-treesitter").install(to_install)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(ev)
+        pcall(vim.treesitter.start, ev.buf)
+        vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
